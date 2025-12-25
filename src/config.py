8a54153cae8c8877a294ce26bf0865ae2e5dcd6f
Mohamed Ayman <88878267+mohamed-ayman-28@users.noreply.github.com>
2025-12-25 16:04:15 +0200

    Delete src/config.py

--- src/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    MAX_LEN = 200        # Max characters per sentence
-    BATCH_SIZE = 64
-    EMBED_DIM = 300      # Match FastText dimension
-    HIDDEN_DIM = 256     # LSTM Hidden size
-    LR = 0.001
-    N_EPOCHS = 10
-    DEVICE = "cuda"
